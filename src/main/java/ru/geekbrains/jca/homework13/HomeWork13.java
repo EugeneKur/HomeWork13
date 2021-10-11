c92a924a0620c1804c35a0a91a6e643b8aa6204f
EugeneKur <ew.kurilov@gmail.com>
2021-10-11 07:49:35 +0300

    Comments

---
+++ src/main/java/ru/geekbrains/jca/homework13/HomeWork13.java
@@ -0,0 +1,30 @@
+package ru.geekbrains.jca.homework13;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.*;
+
+public class HomeWork13 {
+    public static final int CARS_COUNT = 4;
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        CyclicBarrier barrier = new CyclicBarrier(CARS_COUNT+1);
+        CountDownLatch cdl = new CountDownLatch(CARS_COUNT);
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10), barrier, cdl);
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        try {
+            barrier.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+            barrier.await();
+            barrier.await();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
